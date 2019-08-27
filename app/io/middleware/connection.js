e889e912c6a3ac8e903510d7995ec3255df9348c
visom <zhang1zhe2@139.com>
2019-08-27 20:29:56 +0800

    删除多余代码

--- app/io/middleware/connection.js
+++ app/io/middleware/connection.js
@@ -1,8 +1,6 @@
 'use strict';
 module.exports = app => {
   return async (ctx, next) => {
-    ctx.socket.emit('event', 'connected! ' + ctx.socket.id);
-    ctx.socket.join('room');
     await next();
     delete app.clients[ctx.socket.id];
     console.log('disconnection!');
